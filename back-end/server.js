0d455a64fff9a4c911463e6194a6b733413bd27b
dave2000sang <david2000sang@hotmail.com>
2019-05-31 19:56:14 -0400

    wip

--- back-end/server.js
+++ back-end/server.js
@@ -19,24 +19,23 @@ admin.initializeApp({
 })
 
 
+// Initialize Firebase
+var firebaseConfig = {
+	apiKey: "AIzaSyB9tiUIhIC_R9mAibHA71A8WM1Mt9euL0w",
+	authDomain: "contrail-db.firebaseapp.com",
+	databaseURL: "https://contrail-db.firebaseio.com",
+	projectId: "contrail-db",
+	storageBucket: "contrail-db.appspot.com",
+	messagingSenderId: "342081308461",
+	appId: "1:342081308461:web:7b8de20f963649bc"
+};
 
-// // Initialize Firebase
-// var firebaseConfig = {
-// 	apiKey: "AIzaSyB9tiUIhIC_R9mAibHA71A8WM1Mt9euL0w",
-// 	authDomain: "contrail-db.firebaseapp.com",
-// 	databaseURL: "https://contrail-db.firebaseio.com",
-// 	projectId: "contrail-db",
-// 	storageBucket: "contrail-db.appspot.com",
-// 	messagingSenderId: "342081308461",
-// 	appId: "1:342081308461:web:7b8de20f963649bc"
-// };
-
-// firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 
-// // parsing middleware
-// app.use(bodyParser.urlencoded({ extended:true}))
-// app.use(bodyParser.json())
+// parsing middleware
+app.use(bodyParser.urlencoded({ extended:true}))
+app.use(bodyParser.json())
 
 
 app.post('/register', (req, res) => {
@@ -44,42 +43,55 @@ app.post('/register', (req, res) => {
 	var register_email = req.body.register_email
 	var register_password = req.body.register_password
 
-	// do firebase auth queries
-	admin.auth().createUser ({
-		email: register_email,
-		password: register_password,
-		displayName: register_name
-	})
-	.then(function(userRecord) {
-		console.log('Successfully created new user: ', userRecord.uid)
+	// admin.auth().createUser ({
+	// 	email: register_email,
+	// 	password: register_password,
+	// 	displayName: register_name
+	// })
+	// .then(function(userRecord) {
+	// 	console.log('Successfully created new user: ', userRecord.uid)
 
-		res.send({
-			"status" : "registration_success",
-			"name" : userRecord.displayName,
-			"email" : userRecord.email,
-			"password" : userRecord.password,
-			"UID" : userRecord.uid
-		})
-	})
-	.catch(function(error) {
-		console.log('Error creating new user: ', error)
-		res.send({
-			"status" : "registration_fail",
-			"erro" : error
-		})
-	})
+	// 	res.send({
+	// 		"status" : "registration_success",
+	// 		"name" : userRecord.displayName,
+	// 		"email" : userRecord.email,
+	// 		"password" : userRecord.password,
+	// 		"UID" : userRecord.uid
+	// 	})
+	// })
+	// .catch(function(error) {
+	// 	console.log('Error creating new user: ', error)
+	// 	res.send({
+	// 		"status" : "registration_fail",
+	// 		"error" : error
+	// 	})
+	// })
 
+	// create user using Firebase Auth SDK
+	firebase.auth().createUserWithEmailAndPassword(register_email, register_password).catch(functino(error) {
+		var errorCode = error.code
+		var errorMessage = error.message
+		// handle error
+		console.log("create user failed with error: ", errorMessage)
+	})
 })
 
 app.post('/login', (req, res) => {
 	var login_email = req.body.login_email
 	var login_password = req.body.login_password
 
-	// do firebase auth queries
-	admin.auth().getUserByEmail(login_email).then(function(userRecord)) {
-		if(userRecord.password == )
-	}
+	firebase.auth().signInWithEmailAndPassword(login_email, login_password).catch(function(error) {
+		var errorCode = error.code
+		var errorMessage = error.message
+		// handle error
+		console.log("login failed with error: ", errorMessage)
+	})
+
+
+
 })
+
+// signout ?
 
 
 app.listen(port, () => console.log(`Express running on port ${port}`))
