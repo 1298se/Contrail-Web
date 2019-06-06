2339aab2a7ed9d7fddd7c77616e3dd5c91653dc3
osong <osong@opentext.com>
2019-06-06 11:55:30 -0400

    complete UI for login page

--- public/index.js
+++ public/index.js
@@ -1,31 +1,13 @@
-var login = document.getElementById("login");
-var register = document.getElementById("register");
+// Initialize variables
+var loginToggle = document.getElementById("loginToggle")
+var registerToggle = document.getElementById("registerToggle")
+var loginForm = document.getElementsByClassName("form-login")[0]
+var registerForm = document.getElementsByClassName("form-register")[0]
 
-var login_user_button = document.getElementById("login_user_button");
-var register_user_button = document.getElementById("register_user_button");
-
-var login_form = document.getElementsByClassName("login-form")[0];
-var register_form = document.getElementsByClassName("register-form")[0];
-
-login.addEventListener("click", toggleForm);
-register.addEventListener("click", toggleForm);
-
-login_user_button.addEventListener("click", loginUser);
-register_user_button.addEventListener("click", registerUser);
+loginToggle.addEventListener("click", toggleForm)
+registerToggle.addEventListener("click", toggleForm)
 
 function toggleForm() {
-    login_form.classList.toggle("hidden");
-    register_form.classList.toggle("hidden");
-}
-
-function loginUser() {
-    login_form.submit();
-}
-
-function registerUser() {
-    register_form.submit();
-}
-
-function validateInput() {
-    
+    loginForm.classList.toggle("hidden")
+    registerForm.classList.toggle("hidden")
 }
