49a58f742e7c16d4f7432273404fb73e7f79308a
1298se <oliversong7h@gmail.com>
2019-06-07 00:59:57 -0400

    user registration complete

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,6 @@
-// Initialize variables
+/*
+For toggling between displaying loginForm or registerForm
+*/
 var loginToggle = document.getElementById("loginToggle")
 var registerToggle = document.getElementById("registerToggle")
 var loginForm = document.getElementsByClassName("form-login")[0]
@@ -8,6 +10,26 @@ loginToggle.addEventListener("click", toggleForm)
 registerToggle.addEventListener("click", toggleForm)
 
 function toggleForm() {
+    console.log("clicked toggle")
     loginForm.classList.toggle("hidden")
     registerForm.classList.toggle("hidden")
+}
+
+/*
+Login and signup button functionality
+*/
+var loginButton = document.getElementById("loginButton")
+var registerButton = document.getElementById("registerButton")
+
+registerButton.addEventListener("click", registerUser)
+
+function registerUser() {
+    console.log("registering user")
+    var registerUsername = document.getElementById("registerUsername").value
+    var registerEmail = document.getElementById("registerEmail").value
+    var registerPassword = document.getElementById("registerPassword").value
+
+    firebase.auth().createUserWithEmailAndPassword(registerEmail, registerPassword).catch(function(error) {
+        console.log("fuck")
+    })
 }
