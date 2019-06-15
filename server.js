d5c4bf26d027579152890d9681c29270d6d00ec5
dave2000sang <david2000sang@hotmail.com>
2019-06-15 00:21:47 -0400

    clean up server.js

--- server.js
+++ server.js
@@ -1,33 +1,13 @@
 // server.js is the backend script for the server
-
+const app = express()
 const express = require('express')
 const bodyParser = require('body-parser');
-
-const app = express()
-
-var firebase = require('firebase')
-require('firebase/auth')
 var admin = require('firebase-admin')
 
 const port = 3000
 const host = '127.0.0.1'
 
 const serviceAccount = require("C:\\Users\\David\\Downloads\\contrail-db-firebase-adminsdk-vg7u5-38482023bf.json")
-
-
-// Initialize Firebase
-var firebaseConfig = {
-	apiKey: "AIzaSyB9tiUIhIC_R9mAibHA71A8WM1Mt9euL0w",
-	authDomain: "contrail-db.firebaseapp.com",
-	databaseURL: "https://contrail-db.firebaseio.com",
-	projectId: "contrail-db",
-	storageBucket: "contrail-db.appspot.com",
-	messagingSenderId: "342081308461",
-	appId: "1:342081308461:web:7b8de20f963649bc"
-};
-
-firebase.initializeApp(firebaseConfig);
-
 
 // Init db
 admin.initializeApp({
@@ -40,23 +20,10 @@ let db = admin.firestore()
 app.use(bodyParser.urlencoded({ extended:true}))
 app.use(bodyParser.json())
 
-// // allow CORS to communicate between different domains
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
-
 app.use('/', express.static('public'), (req, res, next) => {
 	console.log("static files sent")
 	next()
 })
-
-// app.get('/', (req, res) => {
-// 	console.log("root received")
-// 	res.sendFile(__dirname + "/" + "../public/index.html")
-// 	next()
-// })
 
 app.get('/register', (req, res) => {
 	try {
@@ -64,11 +31,11 @@ app.get('/register', (req, res) => {
 	}
 	catch(error) {
 		console.log("error getting token ", error)
+		res.status(400).send('Bad Request')
 	}
 	admin.auth().verifyIdToken(token)
 		.then(function(decodedToken) {
-			const uid = decodedToken.uid
-			return admin.auth().getUser(uid)
+			return admin.auth().getUser(decodedToken.uid)
 		})
 		.then(function(userRecord) {
 			let new_user = db.collection('users').doc(userId)
