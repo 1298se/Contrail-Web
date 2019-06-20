8140b3e953a17521144d8520853982e303b59d95
Seong Min Park <psm03120@gmail.com>
2019-06-19 20:39:09 -0400

    Installed eslint

--- contrail-ui/src/App.js
+++ contrail-ui/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Redirect } from 'react-router-dom'
-import Auth from './component/auth'
-import Files from './component/files'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import Auth from './component/auth';
+import Files from './component/files';
 
 function App() {
   return (
     <Router>
-          <Route path="/auth" exact component={Auth} />
-          <Route path="/app/files" exact component={Files} />
-          <Redirect to='/auth'/>
+      <Route path="/auth" exact component={Auth} />
+      <Route path="/app/files" exact component={Files} />
+      <Redirect to="/auth" />
     </Router>
   );
 }
