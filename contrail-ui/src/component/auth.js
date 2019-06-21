7a69169087d9dd2c844d78aca8f85040f9f94f83
osong <osong@opentext.com>
2019-06-21 14:39:56 -0400

    Revert "WIP: auth page" added materialUI This reverts commit 278c88ce345ced23adfd8d29cc526f0731158a75.

---
+++ contrail-ui/src/component/auth.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Auth extends Component {
+      
+
+    render() {
+      return (
+       <h1>Login</h1>
+      );
+    }
+}
+
+export default Auth;
