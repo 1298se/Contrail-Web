699694710ba260dc458569aeb7afe3e55cb212f3
1298se <oliversong7h@gmail.com>
2019-06-28 20:39:41 -0400

    fix tab spacing

--- contrail-ui/src/components/main/main-tool-bar/MainToolBar.tsx
+++ contrail-ui/src/components/main/main-tool-bar/MainToolBar.tsx
@@ -15,103 +15,103 @@ import { IMainToolBarProps, IMainToolBarState } from "./mainToolBar.type";
 import styles from "./toolbarStyles";
 
 class MainToolBar extends Component<IMainToolBarProps, IMainToolBarState> {
-  public state = {
-    anchorEl: null,
-    mobileMoreAnchorEl: null,
-  };
+	public state = {
+		anchorEl: null,
+		mobileMoreAnchorEl: null,
+	};
 
-  public handleMobileMenuClose = () => {
-    this.setState({
-      mobileMoreAnchorEl: null,
-    });
-  }
+	public handleMobileMenuClose = () => {
+		this.setState({
+			mobileMoreAnchorEl: null,
+		});
+	}
 
-  public handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    this.setState({
-      mobileMoreAnchorEl: event.currentTarget,
-    });
-  }
+	public handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+		this.setState({
+			mobileMoreAnchorEl: event.currentTarget,
+		});
+	}
 
-  public render() {
-    const mobileMenuId = "toolbar-icons-menu-mobile";
-    const isMobileMenuOpen = Boolean(this.state.mobileMoreAnchorEl);
+	public render() {
+		const mobileMenuId = "toolbar-icons-menu-mobile";
+		const isMobileMenuOpen = Boolean(this.state.mobileMoreAnchorEl);
 
-    const { classes } = this.props;
-    const renderMobileMenu = (
-      <Menu
-        anchorEl={this.state.mobileMoreAnchorEl}
-        anchorOrigin={{ vertical: "top", horizontal: "right" }}
-        id={mobileMenuId}
-        keepMounted={true}
-        transformOrigin={{ vertical: "top", horizontal: "right" }}
-        open={isMobileMenuOpen}
-        onClose={this.handleMobileMenuClose}
-      >
-        <MenuItem onClick={this.handleMobileMenuClose}>
-          <IconButton color="default">
-            <RemoveRedEyeIcon />
-          </IconButton>
-          <p>View</p>
-        </MenuItem>
-        <MenuItem onClick={this.handleMobileMenuClose}>
-          <IconButton color="default">
-            <FavoriteIcon />
-          </IconButton>
-          <p>Favorite</p>
-        </MenuItem>
-        <MenuItem onClick={this.handleMobileMenuClose}>
-          <IconButton color="default">
-            <SharedIcon />
-          </IconButton>
-          <p>Share</p>
-        </MenuItem>
-        <MenuItem onClick={this.handleMobileMenuClose}>
-          <IconButton color="default">
-            <TrashIcon />
-          </IconButton>
-          <p>Trash</p>
-        </MenuItem>
-      </Menu>
-    );
-    return (
-      <div className={classes.grow}>
-        <AppBar position="static" color="secondary">
-          <Toolbar>
-            <Typography className={classes.title} variant="h6" noWrap={true}>
-              Files
+		const { classes } = this.props;
+		const renderMobileMenu = (
+			<Menu
+				anchorEl={this.state.mobileMoreAnchorEl}
+				anchorOrigin={{ vertical: "top", horizontal: "right" }}
+				id={mobileMenuId}
+				keepMounted={true}
+				transformOrigin={{ vertical: "top", horizontal: "right" }}
+				open={isMobileMenuOpen}
+				onClose={this.handleMobileMenuClose}
+			>
+				<MenuItem onClick={this.handleMobileMenuClose}>
+					<IconButton color="default">
+						<RemoveRedEyeIcon />
+					</IconButton>
+					<p>View</p>
+				</MenuItem>
+				<MenuItem onClick={this.handleMobileMenuClose}>
+					<IconButton color="default">
+						<FavoriteIcon />
+					</IconButton>
+					<p>Favorite</p>
+				</MenuItem>
+				<MenuItem onClick={this.handleMobileMenuClose}>
+					<IconButton color="default">
+						<SharedIcon />
+					</IconButton>
+					<p>Share</p>
+				</MenuItem>
+				<MenuItem onClick={this.handleMobileMenuClose}>
+					<IconButton color="default">
+						<TrashIcon />
+					</IconButton>
+					<p>Trash</p>
+				</MenuItem>
+			</Menu>
+		);
+		return (
+			<div className={classes.grow}>
+				<AppBar position="static" color="secondary">
+					<Toolbar>
+						<Typography className={classes.title} variant="h6" noWrap={true}>
+							Files
                 </Typography>
-            <div className={classes.grow} />
-            <div className={classes.sectionDesktop}>
-              <IconButton color="default">
-                <RemoveRedEyeIcon />
-              </IconButton>
-              <IconButton color="default">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton color="default">
-                <SharedIcon />
-              </IconButton>
-              <IconButton
-                edge="end"
-                color="default"
-              >
-                <TrashIcon />
-              </IconButton>
-            </div>
-            <div className={classes.sectionMobile}>
-              <IconButton
-                onClick={this.handleMobileMenuOpen}
-                color="default"
-              >
-                <MoreIcon />
-              </IconButton>
-            </div>
-          </Toolbar>
-        </AppBar>
-        {renderMobileMenu}
-      </div>
-    );
-  }
+						<div className={classes.grow} />
+						<div className={classes.sectionDesktop}>
+							<IconButton color="default">
+								<RemoveRedEyeIcon />
+							</IconButton>
+							<IconButton color="default">
+								<FavoriteIcon />
+							</IconButton>
+							<IconButton color="default">
+								<SharedIcon />
+							</IconButton>
+							<IconButton
+								edge="end"
+								color="default"
+							>
+								<TrashIcon />
+							</IconButton>
+						</div>
+						<div className={classes.sectionMobile}>
+							<IconButton
+								onClick={this.handleMobileMenuOpen}
+								color="default"
+							>
+								<MoreIcon />
+							</IconButton>
+						</div>
+					</Toolbar>
+				</AppBar>
+				{renderMobileMenu}
+			</div>
+		);
+	}
 }
 
 export default withStyles(styles)(MainToolBar);
