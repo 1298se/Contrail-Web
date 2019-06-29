e5dc9ebe6cfddfa685658c55ae7d2ad2e5b435be
1298se <oliversong7h@gmail.com>
2019-06-29 00:30:20 -0400

    WIP: ResourceListView

--- contrail-ui/src/components/main/main-view/MainView.tsx
+++ contrail-ui/src/components/main/main-view/MainView.tsx
@@ -5,17 +5,21 @@ import { borders } from "@material-ui/system";
 import React, { Component } from "react";
 import MainToolBar from "../main-tool-bar/MainToolBar";
 import styles from "../mainStyles";
+import ResourceListView from "../resource-list-view/ResourceListView";
+import Container from "@material-ui/core/Container";
 
 class MainView extends Component<any, any> {
     public render() {
         const { classes } = this.props;
         return (
-            <main className={classes.content}>
+            <div className={classes.content}>
                 <div className={classes.appBarSpacer} />
-                <Box className={classes.mainToolBarBorder}>
+                <Container className={classes.container}>
                     <MainToolBar />
-                </Box>
-            </main>
+                    <div className={classes.appBarSpacer} />
+                    <ResourceListView />
+                </Container>
+            </div>
         );
     }
 }
