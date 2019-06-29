98f5e0157428ae0d9c083b887f94f2b7028641ea
1298se <oliversong7h@gmail.com>
2019-06-29 11:15:18 -0400

    WIP: ResourceListview

--- contrail-ui/src/components/main/main-view/MainView.tsx
+++ contrail-ui/src/components/main/main-view/MainView.tsx
@@ -1,12 +1,12 @@
 import { withStyles } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
 import Divider from "@material-ui/core/Divider";
 import { borders } from "@material-ui/system";
 import React, { Component } from "react";
 import MainToolBar from "../main-tool-bar/MainToolBar";
 import styles from "../mainStyles";
 import ResourceListView from "../resource-list-view/ResourceListView";
-import Container from "@material-ui/core/Container";
 
 class MainView extends Component<any, any> {
     public render() {
