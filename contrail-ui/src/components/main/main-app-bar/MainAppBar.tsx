5eed146f62717a855347a9c3fcc1d1c9455be582
1298se <oliversong7h@gmail.com>
2019-07-14 23:57:22 -0400

    WIP: logout error handling

--- contrail-ui/src/components/main/main-app-bar/MainAppBar.tsx
+++ contrail-ui/src/components/main/main-app-bar/MainAppBar.tsx
@@ -2,6 +2,7 @@ import { IconButton } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar"
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import AccountCircle from "@material-ui/icons/AccountCircle";
@@ -9,22 +10,28 @@ import MenuIcon from "@material-ui/icons/Menu";
 import { withStyles } from "@material-ui/styles";
 import React, { Component } from "react";
 import * as auth from "../../../utils/firebase/auth-utils";
+import SnackbarContentWrapper from "../../feedback/snackbar-content-wrapper/SnackbarContentWrapper";
 import styles from "./appBarStyles";
 import { IMainAppBarProps, IMainAppBarState } from "./mainAppBar.type";
 
+// TODO: Currently, it logs the user out because the authStateChangedListener responds to network failure
+// and redirects the user to login. Needs to be changed to redirect back to login page AFTER
+// logout is complete (e.g.) do not let a redux state update if the user disconnects from WIFI
 class MainAppBar extends Component<IMainAppBarProps, IMainAppBarState> {
     public state = {
         anchorEl: null,
+        logoutRequestError: null,
+        shouldDisplayError: false,
     };
 
     public handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-        this.setState ({
+        this.setState({
             anchorEl: event.currentTarget,
         });
     }
 
     public handleMenuClose = () => {
-        this.setState ({
+        this.setState({
             anchorEl: null,
         });
     }
@@ -32,7 +39,35 @@ class MainAppBar extends Component<IMainAppBarProps, IMainAppBarState> {
     public handleLogoutClick = () => {
         // TODO: Handle Errors
         this.handleMenuClose();
-        auth.logoutUser();
+        auth.logoutUser()
+        .then(() => {
+            this.setState({
+                logoutRequestError: null,
+                shouldDisplayError: false,
+            });
+        }).catch((error) => {
+            this.setState({
+                logoutRequestError: error,
+                shouldDisplayError: true,
+            });
+        });
+    }
+
+    // This function is to handle a bug where the error message of the snackbar
+    // changes during exit transition. This function handles closing the snackbar
+    public handleErrorClose = () => {
+        this.setState({
+            shouldDisplayError: false,
+        });
+    }
+
+    // This function is to handle a bug where the error message of the snackbar
+    // changes during exit transition. This function resets the logoutRequestError to null
+    // after the transition has been completed.
+    public clearLogoutRequestError = () => {
+        this.setState({
+            logoutRequestError: null,
+        });
     }
 
     public render() {
@@ -54,20 +89,34 @@ class MainAppBar extends Component<IMainAppBarProps, IMainAppBarState> {
         );
 
         return (
-            <AppBar position="fixed" className={classes.appBar}>
-                <Toolbar>
-                    <IconButton edge="start" className={classes.drawerButton} color="inherit" >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography className={classes.appBarTitle} variant="h6" noWrap={true}>
-                        Contrail Web
+            <React.Fragment>
+                <Snackbar
+                    anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                    open={this.state.shouldDisplayError}
+                    onClose={this.handleErrorClose}
+                    onExited={this.clearLogoutRequestError}
+                >
+                    <SnackbarContentWrapper
+                        message={String(this.state.logoutRequestError)}
+                        variant="error"
+                        onClose={this.handleErrorClose}
+                    />
+                </Snackbar>
+                <AppBar position="fixed" className={classes.appBar}>
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.drawerButton} color="inherit" >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography className={classes.appBarTitle} variant="h6" noWrap={true}>
+                            Contrail Web
                     </Typography>
-                    <IconButton edge="end" color="inherit" onClick={this.handleMenuOpen}>
-                        <AccountCircle />
-                    </IconButton>
-                </Toolbar>
-                {userMenu}
-            </AppBar>
+                        <IconButton edge="end" color="inherit" onClick={this.handleMenuOpen}>
+                            <AccountCircle />
+                        </IconButton>
+                    </Toolbar>
+                    {userMenu}
+                </AppBar>
+            </React.Fragment>
         );
     }
 }
--- contrail-ui/src/components/main/main-app-bar/mainAppBar.type.ts
+++ contrail-ui/src/components/main/main-app-bar/mainAppBar.type.ts
@@ -3,6 +3,8 @@ import styles from "./appBarStyles";
 
 export interface IMainAppBarState {
     anchorEl: HTMLElement | null;
+    logoutRequestError: string | null;
+    shouldDisplayError: boolean;
 }
 
 export interface IMainAppBarOwnProps extends WithStyles<typeof styles> {
