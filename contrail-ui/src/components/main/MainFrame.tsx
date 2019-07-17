4a48739b812b62d5f78618c769623601ffaea673
1298se <oliversong7h@gmail.com>
2019-07-16 23:02:54 -0400

    wip: adding styles for network disconnected dialog

--- contrail-ui/src/components/main/MainFrame.tsx
+++ contrail-ui/src/components/main/MainFrame.tsx
@@ -1,18 +1,24 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { withStyles } from "@material-ui/styles";
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { IAppReduxState } from "../../store/store.types";
 import MainAppBar from "./main-app-bar/MainAppBar";
 import MainDrawer from "./main-drawer/MainDrawer";
 import MainView from "./main-view/MainView";
+import * as types from "./mainFrame.type";
 import styles from "./mainStyles";
 import NetworkDisconnectedDialog from "./network-disconnected-dialog/NetworkDisconnectedDialog";
 
-class MainFrame extends Component <any, any> {
+class MainFrame extends Component <types.MainFrameProps, {}> {
     public render() {
         const { classes } = this.props;
         return (
             <div className={classes.root}>
-                <NetworkDisconnectedDialog message={"network disconnected"} isOpen={true} />
+                <NetworkDisconnectedDialog
+                    message={"Network Disconnected. Attempting to Reconnect..."}
+                    isOpen={!this.props.hasInternetConnection}
+                />
                 <CssBaseline />
                 <MainAppBar />
                 <MainDrawer />
@@ -21,4 +27,11 @@ class MainFrame extends Component <any, any> {
         );
     }
 }
-export default withStyles(styles)(MainFrame);
+
+const mapStateToProps = (state: IAppReduxState): types.IMainFrameStateProps => {
+    return {
+        hasInternetConnection: state.networkState.hasInternetConnection,
+    };
+};
+
+export default connect(mapStateToProps, null)(withStyles(styles)(MainFrame));
--- contrail-ui/src/components/main/network-disconnected-dialog/NetworkDisconnectedDialog.tsx
+++ contrail-ui/src/components/main/network-disconnected-dialog/NetworkDisconnectedDialog.tsx
@@ -1,27 +1,21 @@
+import { DialogContent } from "@material-ui/core";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { blue } from "@material-ui/core/colors";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import { makeStyles } from "@material-ui/core/styles";
-import PropTypes from "prop-types";
 import React from "react";
 import * as types from "./networkDisconnectedDialog.types";
 
-const useStyles = makeStyles({
-  avatar: {
-    backgroundColor: blue[100],
-    color: blue[600],
-  },
-});
-
 function SimpleDialog(props: types.NetworkDisconnectedProps) {
-  const classes = useStyles();
   const { message, isOpen } = props;
 
   return (
     <Dialog open={isOpen}>
       <DialogTitle id="simple-dialog-title">{message}</DialogTitle>
-        <CircularProgress color="primary" />
+      <DialogContent>
+        <CircularProgress />
+      </DialogContent>
     </Dialog>
   );
 }
