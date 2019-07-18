f87553a8aa2023a83d2a540ed337ebbd5f552c34
osong <osong@opentext.com>
2019-07-18 13:53:37 -0400

    integrating firebase functions for backend

--- contrail-api/src/firebaseapp.ts
+++
@@ -1,9 +0,0 @@
-
-import * as admin from "firebase-admin";
-import serviceAccount from "./ServiceAccountKey";
-
-const app = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
-
-export default app;
