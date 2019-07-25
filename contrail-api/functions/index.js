38bfaa7e739d96a47364a82a22c18f34bc7f50c7
1298se <oliversong7h@gmail.com>
2019-07-24 22:48:52 -0400

    removed registration from api

--- contrail-api/functions/index.js
+++ contrail-api/functions/index.js
@@ -1,11 +1,8 @@
 const functions = require('firebase-functions');
 const app = require('express')();
-const { registerUser } = require("./handlers/auth");
 
 app.get("/", (req, res) => {
     res.status(200).send({message: "Welcome to Contrail"});
 })
-
-app.post("/register", registerUser);
 
 exports.api = functions.https.onRequest(app);
