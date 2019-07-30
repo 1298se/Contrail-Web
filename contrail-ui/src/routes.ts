0da28fc75260ddb367457695b69779f723e10d62
osong <osong@opentext.com>
2019-07-30 13:16:25 -0400

    setup resourcelist for resource props

--- contrail-ui/src/routes.ts
+++ contrail-ui/src/routes.ts
@@ -2,7 +2,3 @@ export const ROOT = "/";
 export const MAIN = "/app";
 export const LOGIN = "/login";
 export const REGISTER = "/register";
-export const FILES = "/app/files";
-export const FAVORITES = "/app/favorites";
-export const SHARED = "/app/shared";
-export const TRASH = "/app/trash";
