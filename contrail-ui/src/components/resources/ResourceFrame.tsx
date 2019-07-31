d4bed88e007fbf9cebbadc60bee9e18010c4b342
osong <osong@opentext.com>
2019-07-31 17:03:40 -0400

    adding error handling

--- contrail-ui/src/components/resources/ResourceFrame.tsx
+++ contrail-ui/src/components/resources/ResourceFrame.tsx
@@ -1,3 +1,4 @@
+import Snackbar from "@material-ui/core/Snackbar";
 import { withStyles } from "@material-ui/styles";
 import React, { Component } from "react";
 import { connect } from "react-redux";
@@ -6,16 +7,55 @@ import { ThunkDispatch } from "redux-thunk";
 import { fetchRootResources } from "../../store/actions/resourceActions";
 import { IResourceFetchAllAction } from "../../store/actions/resourceActions.types";
 import { IAppReduxState } from "../../store/store.types";
+import SnackbarContentWrapper from "../feedback/snackbar-content-wrapper/SnackbarContentWrapper";
 import ResourceListView from "./resource-list-view/ResourceListView";
 import ResourceToolBar from "./resource-tool-bar/ResourceToolBar";
 import * as types from "./resourceFrame.types";
 import styles from "./resourceStyles";
 
-class ResourceFrame extends Component<types.ResourceFrameProps, {}> {
+class ResourceFrame extends Component<types.ResourceFrameProps, types.IResourceFrameState> {
+    public state: types.IResourceFrameState = {
+        snackbarDisplay: {
+            snackbarVariant: "error",
+            snackbarMessage: null,
+            shouldDisplaySnackbar: false,
+        },
+    };
+
     public componentDidMount() {
         this.props.fetchRootResources()
-        .catch((error) => {
-            // TODO: Handle error with Snackbar
+            .catch((error) => {
+                this.setSnackbarError("Failed to load resources: " + error);
+            });
+    }
+
+    public setSnackbarError = (errorMessage: any) => {
+        this.setState({
+            snackbarDisplay: {
+                snackbarVariant: "error",
+                snackbarMessage: errorMessage,
+                shouldDisplaySnackbar: true,
+            },
+        });
+    }
+
+    // For closing an opened Snackbar. Must be executed first before clearing the snackbar message.
+    public handleSnackbarClose = () => {
+        this.setState({
+            snackbarDisplay: {
+                ...this.state.snackbarDisplay,
+                shouldDisplaySnackbar: false,
+            },
+        });
+    }
+
+    // Clears the snackbar message.
+    public clearSnackbarMessage = () => {
+        this.setState({
+            snackbarDisplay: {
+                ...this.state.snackbarDisplay,
+                snackbarMessage: null,
+            },
         });
     }
 
@@ -24,10 +64,11 @@ class ResourceFrame extends Component<types.ResourceFrameProps, {}> {
         const { userResources } = this.props;
         const { match } = this.props;
         const sharedResources = userResources.sharedBy.concat(userResources.sharedTo);
+        const { snackbarVariant, snackbarMessage, shouldDisplaySnackbar } = this.state.snackbarDisplay;
 
         const renderRootResources = () => <ResourceListView display={userResources.root} />;
         const renderFavouriteResources = () => <ResourceListView display={userResources.favourites} />;
-        const renderSharedResources =  () => <ResourceListView display={sharedResources} />;
+        const renderSharedResources = () => <ResourceListView display={sharedResources} />;
         const renderTrashResources = () => <ResourceListView display={userResources.trash} />;
 
         return (
@@ -35,6 +76,18 @@ class ResourceFrame extends Component<types.ResourceFrameProps, {}> {
                 <ResourceToolBar />
                 <div className={classes.appBarSpacer} />
                 <Router>
+                    <Snackbar
+                        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                        open={shouldDisplaySnackbar}
+                        onClose={this.handleSnackbarClose}
+                        onExited={this.clearSnackbarMessage}
+                    >
+                        <SnackbarContentWrapper
+                            message={String(snackbarMessage)}
+                            variant={snackbarVariant}
+                            onClose={this.handleSnackbarClose}
+                        />
+                    </Snackbar>
                     <Route path={`${match.path}/files`} render={renderRootResources} />
                     <Route path={`${match.path}/favourites`} render={renderFavouriteResources} />
                     <Route path={`${match.path}/shared`} render={renderSharedResources} />
--- contrail-ui/src/components/resources/resourceFrame.types.ts
+++ contrail-ui/src/components/resources/resourceFrame.types.ts
@@ -1,6 +1,7 @@
 import { WithStyles } from "@material-ui/core";
 import { RouteComponentProps } from "react-router";
 import { IUserResources } from "../../types/resource.types";
+import { ISnackbarDisplay } from "../feedback/snackbar-content-wrapper/snackbarContentWrapper.types";
 import styles from "./resourceStyles";
 
 enum ResourceRoutes {
@@ -22,6 +23,10 @@ export interface IResourceFrameDispatchProps {
 
 export interface IResourceFrameOwnProps extends WithStyles<typeof styles>, RouteComponentProps<IMatchParams> {
 
+}
+
+export interface IResourceFrameState {
+    snackbarDisplay: ISnackbarDisplay;
 }
 
 export type ResourceFrameProps =
